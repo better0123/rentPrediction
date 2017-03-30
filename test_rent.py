a54b63ce7f61e1bf91a82790971134bc98455e58
yh1008 <ye.hua@columbia.edu>
2017-03-29 23:57:37 -0400

    Update test_rent.py

--- test_rent.py
+++ test_rent.py
@@ -1,7 +1,7 @@
 """This module is for testing the trained result reached our claimed R2 value
 """
 
-from homework2_rent import score_rent
+from predict_rent import score_rent
 
 def test_rent():
     r"""Test the trained model with the claimed R2 value
